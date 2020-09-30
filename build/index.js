26b99a3715c5bf5ef7f30966fa8bd09573d47817
Jacky <jacky.lin@hwaotech.com>
2020-09-30 14:19:09 +0800

    feat:多版本4

--- build/index.js
+++ build/index.js
@@ -1,14 +1,14 @@
 const argv = require('minimist')(process.argv.slice(2));
 const { spawn } = require('child_process');
 
+const command = argv.c || 'serve';
 const plat = argv.p || 'demo';
+
 const env = {
-  ...Object.create(process.env),
-  NODE_ENV: 'production',
   PATH: process.env.PATH,
 };
 
-const ls = spawn('vue-cli-service', ['build', '--mode', plat, '--color'], { env });
+const ls = spawn(`vue-cli-service`, [command, '--mode', plat, '--color'], { env });
 ls.stdout.on('data', data => {
   console.log(`${data}`);
 });
