896d91e38be5a41bb66492b03f783492f992cd3a
Jacky <jacky.lin@hwaotech.com>
2020-09-30 13:15:37 +0800

    feat:多版本2

---
+++ build/index.js
@@ -0,0 +1,18 @@
+const argv = require('minimist')(process.argv.slice(2));
+const { spawn } = require('child_process');
+const plat = argv.p || 'demo';
+const env = {
+  ...Object.create(process.env),
+  NODE_ENV: 'production',
+  PATH: process.env.PATH,
+};
+const ls = spawn('vue-cli-service', ['build', '--mode', plat], { env });
+ls.stdout.on('data', data => {
+  console.log(`${data}`);
+});
+ls.stderr.on('data', err => {
+  console.error(`${err}`);
+});
+ls.on('close', code => {
+  console.log(`error code: ${code}`);
+});
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-const { VUE_APP_TEMPLATE_TYPE, VUE_APP_TEMPLATE_NUMBER } = process.env;
+const { VUE_APP_PLAT_ID, VUE_APP_TEMPLATE_TYPE, VUE_APP_TEMPLATE_NUMBER } = process.env;
 const TEMPLATE = VUE_APP_TEMPLATE_TYPE + VUE_APP_TEMPLATE_NUMBER;
+
+console.log('[PLAT]', VUE_APP_PLAT_ID);
 console.log('[TEMPLATE]', TEMPLATE);
 
 module.exports = {
